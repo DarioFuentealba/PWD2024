761d830ed7e4f5d09e68b58d6234b73699b5694f
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-08-22 20:29:10 -0300

    Cambié el nombre del grupo de "Grupo X" a "Grupo 5".

--- Vista/Estructura/header.php
+++ Vista/Estructura/header.php
@@ -31,7 +31,7 @@
 <body>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../../Inicio/Principal/principal.php">Grupo X</a>
+    <a class="navbar-brand" href="../../Inicio/Principal/principal.php">Grupo 5</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
