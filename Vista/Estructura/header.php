0f7d268d448136967273b4124a7c98ee6ba89572
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-08-22 20:24:49 -0300

    Modifiqué estilos.css y header.php para tener un sub-menú desplegable en el ejercicio 2 del TP 2.

--- Vista/Estructura/header.php
+++ Vista/Estructura/header.php
@@ -37,6 +37,8 @@
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav"> 
+
+        <!-- Menu TP 1 -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">TP N° 1</a>
           <ul class="dropdown-menu">
@@ -50,15 +52,34 @@
             <li><a class="dropdown-item" href="../../TP1/Ejercicio/ejercicio8.php">Ejercicio 8</a></li>
           </ul>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">TP N° 2</a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio1.php">Ejercicio 1</a></li>
-            <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2.php">Ejercicio 2</a></li>
-            <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio3.php">Ejercicio 3</a></li>
-            <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio4.php">Ejercicio 4</a></li>
-          </ul>
-        </li>
+
+<!-- Menú TP 2 -->
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">TP N° 2</a>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio1.php">Ejercicio 1</a></li>
+
+    <!-- Submenú Ejercicio 2 TP 2 -->
+    <li class="dropdown-submenu">
+      <a class="dropdown-item dropdown-toggle" href="#">Ejercicio 2</a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2-1.php">Ejercicio 1</a></li>
+        <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2-2.php">Ejercicio 2</a></li>
+        <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2-3.php">Ejercicio 3</a></li>
+        <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2-4.php">Ejercicio 4</a></li>
+        <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2-5.php">Ejercicio 5</a></li>
+        <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2-6.php">Ejercicio 6</a></li>
+        <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2-7.php">Ejercicio 7</a></li>
+        <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio2-8.php">Ejercicio 8</a></li>
+      </ul>
+    </li>
+
+    <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio3.php">Ejercicio 3</a></li>
+    <li><a class="dropdown-item" href="../../TP2/Ejercicio/ejercicio4.php">Ejercicio 4</a></li>
+  </ul>
+</li>
+
+        <!-- Menu TP 3 -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">TP N° 3</a>
           <ul class="dropdown-menu">
