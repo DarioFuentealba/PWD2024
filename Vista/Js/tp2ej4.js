fac05ccb013b466877b3b663be972ba95adf1c1e
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-08-23 18:06:45 -0300

    Mejora en validacion tp2eje4.js

--- Vista/Js/tp2ej4.js
+++ Vista/Js/tp2ej4.js
@@ -5,20 +5,20 @@ $(document).ready(function() {
         
         // Validación del año
         var anio = $("#anio").val().trim();
-        if(anio === "" || isNaN(anio) || anio.length > 4){
+        if(anio === "" || isNaN(anio) || anio.length > 4 || anio < 1895 || anio > 2024){
             verifica = false;
             $("#anio").addClass('is-invalid'); // Agrega clase para borde rojo
             $("#anio").addClass('bg-danger'); // Agrega clase para fondo rojo claro
-            mensaje += "-El año debe ser un número con máximo 4 caracteres.\n";
+            mensaje += "-El año debe ser un número entre 1895 y 2024.\n";
         }
 
         // Validación de duración
         var duracion = $("#duracion").val().trim();
-        if(duracion === "" || isNaN(duracion) || duracion.length > 3){
+        if(duracion === "" || isNaN(duracion) || duracion.length > 3 || duracion < 1 || duracion > 1260){
             verifica = false;
             $("#duracion").addClass('is-invalid'); // Agrega clase para borde rojo
             $("#duracion").addClass('bg-danger'); // Agrega clase para fondo rojo claro            
-            mensaje += "-La duración debe ser un número con máximo 3 caracteres.\n";
+            mensaje += "-La duración debe ser un número entre 1 y 1260.\n";
         }
 
         // Validación de campos obligatorios
