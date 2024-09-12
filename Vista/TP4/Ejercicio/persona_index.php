c972c5432e85e98ef21a5ff5715a469c4459e779
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2024-09-12 20:40:58 -0300

    12-9 20:40

--- Vista/TP4/Ejercicio/persona_index.php
+++ Vista/TP4/Ejercicio/persona_index.php
@@ -6,38 +6,6 @@
     $listaPersona = $objAbmPersona->buscar(null);
 ?>	
 
-<<<<<<< Updated upstream
-<!-- Titulo en la pagina -->
-<h3 class="text-center">Base de datos de personas</h3>
-
-<!-- Boton agregar nueva persona -->
-<a href="persona_nuevo.php">nuevo</a>
-
-<!-- Tabla -->
-<table border="1">
-    <tr>
-        <th style="width:500px;">Dni</th>
-        <th style="width:500px;">Apellido</th>
-        <th style="width:500px;">Nombre</th>
-        <th style="width:500px;">Fecha Nacimiento</th>
-        <th style="width:500px;">Teléfono</th>
-        <th style="width:500px;">Domicilio</th>
-    </tr>
-
-    <?php	
-        if( count($listaPersona) > 0)
-        {
-            foreach ($listaPersona as $objPersona)
-            {
-                echo '<tr><td style="width:500px;">'.$objPersona->getNroDni().'</td>';
-                echo '<td style="width:500px;">'.$objPersona->getApellido().'</td>';
-                echo '<td style="width:500px;">'.$objPersona->getNombre().'</td>';
-                echo '<td style="width:500px;">'.$objPersona->getFechaNac().'</td>';
-                echo '<td style="width:500px;">'.$objPersona->getTelefono().'</td>';
-                echo '<td style="width:500px;">'.$objPersona->getDomicilio().'</td>';
-                echo '<td><a href="persona_editar.php?NroDni='.$objPersona->getNroDni().'">editar</a></td>';
-                echo '<td><a href="../Accion/persona_accion.php?accion=borrar&NroDni='.$objPersona->getNroDni().'">borrar</a></td></tr>'; 
-=======
 <div class="container mt-3">
   <h2>ABM Personas</h2>
   <p>Listado de personas incluidas en la base de datos</p>      
@@ -73,7 +41,6 @@
                 echo '<td>'.$objPersona->getDomicilio().'</td>';
                 echo '<td><a href="persona_editar.php?NroDni='.$objPersona->getNroDni().'" class="btn btn-outline-info btn-sm" role="button">editar</a></td>';
                 echo '<td><a href="../Accion/persona_accion.php?accion=borrar&NroDni='.$objPersona->getNroDni().'" class="btn btn-outline-danger btn-sm" role="button">borrar</a></td></tr>'; 
->>>>>>> Stashed changes
             }
         }
     ?>
